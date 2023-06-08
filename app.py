396c4629e1af3ee205b22462d83e83988f0c02ec
Pepe <95196085+PePeWee07@users.noreply.github.com>
2023-06-08 16:13:46 -0500

    cargado todo

--- app.py
+++ app.py
@@ -10,11 +10,27 @@ from werkzeug.utils import secure_filename
 #from gradcam import *
 #import base64
 
+from libauc.losses import AUCM_MultiLabel, CrossEntropyLoss
+from libauc.optimizers import PESG, Adam
 from libauc.models import densenet121 as DenseNet121
+from libauc.datasets import CheXpert
+
 import torch
+from torch import nn
+from torchvision import transforms
 from PIL import Image
-import cv2
 import numpy as np
+import cv2
+#from torchvision import models
+
+from torchcam.methods import GradCAM,LayerCAM,XGradCAM
+
+from PIL import Image
+
+from torchcam.utils import overlay_mask
+from torchvision.transforms.functional import normalize, resize, to_pil_image
+import matplotlib.pyplot as plt
+from PIL import Image
 
 app = Flask(__name__)
 
@@ -53,6 +69,9 @@ def upload_file():
         print('extension:', extension)
         ext=str(extension)
 
+        #from torchcam.methods import GradCAM,LayerCAM,XGradCAM
+        cam_extractor = GradCAM(model, 'features.norm5')
+
         #Img---------------------------------
         # Lee la imagen enviada desde Postman
         file = request.files['file']  
@@ -77,6 +96,54 @@ def upload_file():
         img= image.transpose((2, 0, 1)).astype(np.float32)
         input_tensor = torch.from_numpy(img)
         input_tensor = torch.unsqueeze(input_tensor, 0)
+
+        #modelo
+        out=model(input_tensor)
+        print(out)
+
+        #0:Cardiomegaly, 1:Edema, 2:Consolidation, 3:Atelectasis, 4:Pleural Effusion
+        for i in range(5):
+            out=model(input_tensor)
+            cams=cam_extractor(i,out)
+            for name, cam in zip(cam_extractor.target_names, cams):
+                cam_cpu = cam.squeeze(0).cpu()  # Move to CPU before converting to PIL image
+                result = overlay_mask(Image.fromarray(imageCSV).convert("RGB"), to_pil_image(cam_cpu, mode='F'), alpha=0.5)
+                #result = overlay_mask(Image.fromarray(image).convert("RGB"), to_pil_image(cam_cpu, mode='F'), alpha=0.5)
+                if(i==0):
+                        plt.imshow(result)
+                        plt.axis('off')
+                        plt.title("Cardiomegaly")
+                        plt.savefig('Cardiomegaly.jpg', format='jpg')
+                        plt.show()
+                        plt.close()
+                if(i==1):
+                        plt.imshow(result)
+                        plt.axis('off')
+                        plt.title("Edema")
+                        plt.savefig('Edema.jpg', format='jpg')
+                        plt.show()
+                        plt.close()
+                if(i==2):
+                        plt.imshow(result)
+                        plt.axis('off')
+                        plt.title("Consolidation")
+                        plt.savefig('Consolidation.jpg', format='jpg')
+                        plt.show()
+                        plt.close()
+                if(i==3):
+                        plt.imshow(result)
+                        plt.axis('off')
+                        plt.title("Atelectasis")
+                        plt.savefig('Atelectasis.jpg', format='jpg')
+                        plt.show()
+                        plt.close()
+                if(i==4):
+                        plt.imshow(result)
+                        plt.axis('off')
+                        plt.title("Pleural Effusion")
+                        plt.savefig('Pleural Effusion.jpg', format='jpg')
+                        plt.show()
+                        plt.close()
         return str(input_tensor)
     else:               
         resp = jsonify({'message' : 'Allowed file types are, png, jpg, jpeg, gif'})
